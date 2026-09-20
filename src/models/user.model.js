82b66530ef164b15adc3390ceb610a3a3a974111
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-20 20:29:22 +0530

    Add user model schema with email validation

---
+++ src/models/user.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    email:{
+        type:String,
+        required:[true,"Email is required for creating a user"],
+        trim:true,
+        lowercase:true,
+        match:[/^[^\s@]+@[^\s@]+\.[^\s@]+$/]
+    },
+    unique:[true,"Email Already exists"]
+})
