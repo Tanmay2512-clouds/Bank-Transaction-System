6b5cf6048da6980b5c9a8bf932c02efa90206c05
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-21 10:56:28 +0530

    Refactor user schema to include name and password fields with validation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -6,7 +6,19 @@ const userSchema = mongoose.Schema({
         required:[true,"Email is required for creating a user"],
         trim:true,
         lowercase:true,
-        match:[/^[^\s@]+@[^\s@]+\.[^\s@]+$/]
+        match:[/^[^\s@]+@[^\s@]+\.[^\s@]+$/],
+        unique:[true,"Email Already exists"]
     },
-    unique:[true,"Email Already exists"]
+    name:{
+        type:String,
+        required:[true,"Name is Required for this account"]
+    },
+    password:{
+        type:String,
+        required:[true,"Password is required for creating the "],
+        minlength:[6,"Password should constain atleast 6 character"],
+        select:false
+
+    }
+    
 })
