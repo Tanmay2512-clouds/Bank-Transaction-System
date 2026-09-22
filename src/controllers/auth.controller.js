0147f054381230c0664999a345961c040b184208
Tanmay2512-clouds <tanmay.chopra2514@gmail.com>
2026-09-22 14:47:34 +0530

    Add jsonwebtoken dependency and implement user registration logic in auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -13,6 +13,10 @@ async function userRegisterController(req,res){
             status:"failed"
         })
     }
+
+    const user = await userModel.create({
+        email , password , name 
+    })
 }
 
 module.exports = {
